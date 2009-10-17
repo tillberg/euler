e9aa251ff406451af7775cb14ec3d878e4afac61
Dan Tillberg <dan@tillberg.us>
2009-10-17 16:04:57 -0400

    Finished problem 2 and starting on 3

--- euler.rb
+++ euler.rb
@@ -3,22 +3,33 @@ class Array; def sum; inject( nil ) { |sum,x| sum ? sum+x : x }; end; end
 
 problem = ARGV[0].to_i
 
-desc = ''
-sol = ''
+desc = nil
+sol = nil
 
 case problem
-  
-  
 when 1
   desc = 'Add all the natural numbers below one thousand that are multiples of 3 or 5.'
   sol = (1...1000).to_a.find_all {|x| x % 3 == 0 || x % 5 == 0 }.sum
-
-
-
+when 2
+  desc = 'Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million.'
+  fib = [1, 2]
+  while true
+    nxt = fib[-1] + fib[-2]
+    break if nxt > 4000000
+    fib.push(nxt)
+  end
+  sol = fib.find_all {|x| x % 2 == 0}.sum
+when 3
+  desc = 'Find the largest prime factor of a composite number.'
+  num = 600851475143
 end
 
-puts
-puts '    Problem: ' + problem.to_s
-puts 'Description: ' + desc
-puts '   Solution: ' + sol.to_s
-puts
+if desc == nil or sol == nil then
+  puts 'That problem number does not yet have a solution.'
+else
+  puts
+  puts '    Problem: ' + problem.to_s
+  puts 'Description: ' + desc
+  puts '   Solution: ' + sol.to_s
+  puts  
+end
