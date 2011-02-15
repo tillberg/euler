ab0b0d066f2864f301e1b85be675a78db431fc55
Dan Tillberg <dan@tillberg.us>
2011-02-15 00:56:55 -0500

    scala finished through number 5

--- euler.scala
+++ euler.scala
@@ -2,6 +2,7 @@
 exec scala "$0" "$@"
 !#
 
+import scala.collection.TraversableLike
 import scala.collection.mutable._
 
 class RichStream[A](str: =>Stream[A]) {
@@ -26,11 +27,22 @@ object Euler {
     }
     case 2 => {
       desc = "Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million."
-      fib.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum
+      even(fib.takeWhile(_ <= 4000000)).sum
     }
     case 3 => {
       desc = "Find the largest prime factor of a composite number."
       factors(600851475143L).max
+    }
+    case 4 => {
+      desc = "Find the largest palindrome made from the product of two 3-digit numbers."
+      val rng = 100 until 1000
+      (for (x <- rng; y <- rng if isPalindrome(x * y)) yield x * y).max
+    }
+    case 5 => {
+      desc = "What is the smallest number divisible by each of the numbers 1 to 20?"
+      val rng = 2 to 20
+      val step = rng.filter(isPrime(_)).reduceLeft(_ * _)
+      naturals.map(_ * step).find(n => rng.forall(i => n % i == 0)).get
     }
   }
   
@@ -51,6 +63,9 @@ object Euler {
     }
     fac
   }
+  def even(t: Stream[Int]) = t.filter(_ % 2 == 0)
+  def isPalindrome(n: Int) = n.toString.reverse == n.toString
+  def isPrime(n: Long) = factors(n).length == 1
 }
 
 Euler.main(args)
